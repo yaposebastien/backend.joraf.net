df8f168ff290a3accbb7461c0e68baf5564b3bc8
yaposebastien <yaposebastien@gmail.com>
2022-12-29 21:53:56 -0500

    Initial folder of the project

---
+++ spring-boot-joraf-microservice-backend/src/main/java/net/joraf/springbootjorafmicroservicebackend/SpringBootJorafMicroserviceBackendApplication.java
@@ -0,0 +1,13 @@
+package net.joraf.springbootjorafmicroservicebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJorafMicroserviceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJorafMicroserviceBackendApplication.class, args);
+	}
+
+}
---
+++ spring-boot-joraf-microservice-backend/src/test/java/net/joraf/springbootjorafmicroservicebackend/SpringBootJorafMicroserviceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.joraf.springbootjorafmicroservicebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJorafMicroserviceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
